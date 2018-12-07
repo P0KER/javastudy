cfa247922c881c9a35273f18dd82569b38f3c505
poker <puke@unionpay.com>
2018-12-07 09:26:50 +0800

    add

---
+++ src/main/java/com/unionpay/io/SystemInDemo.java
@@ -0,0 +1,28 @@
+package com.unionpay.io;
+
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by puke on 2018/12/6
+ */
+public class SystemInDemo {
+    public static void main(String[] args) throws IOException {
+        InputStream input = System.in;
+        //想输入中文需要使用内存字节流来处理
+        ByteOutputStream output = new ByteOutputStream();
+//        StringBuffer buf = new StringBuffer();
+        int temp = 0;
+        while((temp=input.read())!='\n'){
+            /*if(temp!='\n'){
+                System.out.println((char)temp);
+            }*/
+            output.write(temp);
+//            buf.append((char)temp);
+        }
+        System.out.println(new String(output.toByteArray()));
+//        System.out.println(buf.toString());
+    }
+}
